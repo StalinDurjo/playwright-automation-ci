59f90e5a957f2da1af7c5994c2d2f917df88ebc0
Stalin Durjo <stalindurjo@Stalin.local>
2024-05-25 10:45:54 +0600

    mock data support added

---
+++ support/lib/api-request.ts
@@ -0,0 +1,40 @@
+import axios, { AxiosInstance } from "axios";
+import { ContentType } from "support/types/global";
+
+export default class ApiRequest {
+  private instance: AxiosInstance;
+
+  constructor() {
+    this.instance = axios.create();
+  }
+
+  setBearerToken(token: string) {
+    this.instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  }
+
+  setBasicAuth(username: string, password: string) {
+    this.instance.defaults.headers.common["Authorization"] = `Basic ${btoa(`${username}:${password}`)}`;
+  }
+
+  setContentType(contentType: ContentType) {
+    this.instance.defaults.headers.common["Content-Type"] = contentType;
+  }
+
+  get(requestPath: string, queryString: string = "") {
+    return this.instance.get(requestPath, {
+      params: queryString
+    });
+  }
+
+  post(requestPath: string, requestBody: unknown) {
+    return this.instance.post(requestPath, requestBody);
+  }
+
+  put(requestPath: string, requestBody: unknown) {
+    return this.instance.put(requestPath, requestBody);
+  }
+
+  delete(requestPath: string, queryString: string = "") {
+    return this.instance.delete(requestPath, { params: queryString });
+  }
+}
---
+++ support/types/global.d.ts
@@ -0,0 +1,2 @@
+export type Mode = "API" | "UI";
+export type ContentType = "application/json" | "multipart/form-data";
