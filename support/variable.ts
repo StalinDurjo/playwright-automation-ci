d206fb681e3fe037757aaa97b3031ea0679fc497
stalindurjo <stalindurjo@gmail.com>
2024-05-28 22:08:29 +0600

    project variable file added

---
+++ support/variable.ts
@@ -0,0 +1,15 @@
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const ROOT_DIR = path.resolve(process.cwd());
+export const TEST_DIRECTORY = `${ROOT_DIR}/tests`;
+export const MOCK_DATA_DIRECTORY = `${ROOT_DIR}/mock-data`;
+
+// The following directories are created during application runtime
+export const OUT_DIR = `${ROOT_DIR}/out`;
+export const WP_DIR = `${OUT_DIR}/wp`;
+export const REPORT_DIRECTORY = `${ROOT_DIR}/reports`;
+
+export const GLOBAL_ADMIN_USERNAME = process.env.ADMIN_USERNAME;
+export const GLOBAL_ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
